3f15589b7271075829147c47ee00cd092734590c
Omar-Magdy0 <omar.magdy.om.om@gmail.com>
2023-11-27 15:45:28 +0200

    update

--- src/Arduino station.cpp
+++ src/Arduino station.cpp
@@ -6,58 +6,36 @@
 
 // 'BIGANT LOGO', 128x64px
 const unsigned char BIGANT_LOGO [] PROGMEM = {
-
 };
 
 
   
 
-
-// Array of all bitmaps for convenience. (Total bytes used to store images in PROGMEM = 1040)
 lightDisplay dispS = lightDisplay(128,64,&Wire);
 FUNKYSquare S1 = FUNKYSquare(15,12,40,40,&dispS);
 FUNKYSquare S2 = FUNKYSquare(25,12,40,40,&dispS);
-FUNKYSquare S3 = FUNKYSquare(35,12,40,40,&dispS);
-FUNKYSquare S4 = FUNKYSquare(45,12,40,40,&dispS);
-FUNKYSquare S5 = FUNKYSquare(55,12,40,40,&dispS);
-FUNKYSquare S6 = FUNKYSquare(65,12,40,40,&dispS);
-FUNKYSquare S7 = FUNKYSquare(75,12,40,40,&dispS);
-
-
 
 
 
 
    void display1(){
-      S1.DisplayFunction();
-      S2.DisplayFunction();
-      S3.DisplayFunction();
-      S4.DisplayFunction();
-      S5.DisplayFunction();
-      S6.DisplayFunction();
-      S7.DisplayFunction();
-
+    dispS.drawFillRect(0,0,128,64,WHITE);
+    dispS.setCursor(10,40);
+    dispS.println(F("SADISTIC SLAYER"));
    }
 
 int main(){
     clk1CountMillis(START);
     defaultBootLoader();
     Serial.begin(115200);
-    if(dispS.begin()){Serial.println("SCREEN INITIALIZED");}
-    dispS.setCursor(32,12);
+    if(dispS.begin()){Serial.println(F("SCREEN INITIALIZED"));}
+    dispS.setWrap(true);
+    dispS.setTextColor(BLACK);
+    dispS.displayFunctionGroup(0,7,display1);
     while(1){
-      S1.angle+= 0.05;
-      S2.angle+= 0.15;
-      S3.angle+= 0.2;
-      S4.angle+= 0.25;
-      S5.angle+= 0.3;
-      S6.angle+= 0.35;
-      S7.angle+= 0.4;
+          _delay_ms(100);
+  
 
-            dispS.displayFunctionGroup(0,7,display1);
-
-
-    
 /*      
         atomicDebounceReEnable(); 
         if(APMNGCTRL & (1<<APRNNG)){
