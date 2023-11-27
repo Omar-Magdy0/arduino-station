88c4b1710f52818031061e8d825610c33054ef8e
Omar-Magdy0 <omar.magdy.om.om@gmail.com>
2023-11-27 19:56:28 +0200

    update

--- src/Arduino station.cpp
+++ src/Arduino station.cpp
@@ -2,38 +2,48 @@
 #include "applicationManager.h"
 #include <lightDisplay.h>
 #include "FunkyAnimations/FunkyObjects.h"
-
+#include "WString.h"
 
 // 'BIGANT LOGO', 128x64px
 const unsigned char BIGANT_LOGO [] PROGMEM = {
 };
-
-
-  
 
 lightDisplay dispS = lightDisplay(128,64,&Wire);
 FUNKYSquare S1 = FUNKYSquare(15,12,40,40,&dispS);
 FUNKYSquare S2 = FUNKYSquare(25,12,40,40,&dispS);
 
 
+    int16_t a,b;uint8_t c,d;
+    int16_t x = 60,y = 8;
+
+
 
 
    void display1(){
-    dispS.drawFillRect(0,0,128,64,WHITE);
-    dispS.setCursor(10,40);
-    dispS.println(F("SADISTIC SLAYER"));
+    dispS.setCursor(x,y);
+    dispS.drawFillRect(x - 1,y - d,c + 2,d + 2,WHITE);
+        dispS.print(F("SAD"));
    }
+
+
 
 int main(){
     clk1CountMillis(START);
     defaultBootLoader();
     Serial.begin(115200);
     if(dispS.begin()){Serial.println(F("SCREEN INITIALIZED"));}
+    dispS.getTextBounds(F("SAD"),x,y,&a,&b,&c,&d);
     dispS.setWrap(true);
+    Serial.println(a);
+    Serial.println(b);
+    Serial.println(c);
+    Serial.println(d);
     dispS.setTextColor(BLACK);
     dispS.displayFunctionGroup(0,7,display1);
     while(1){
           _delay_ms(100);
+            dispS.displayFunctionGroup(0,7,display1);
+            y++;
   
 
 /*      
